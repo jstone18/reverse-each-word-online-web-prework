b900574e38e047e26a4137015b54459f2413f2ae
Jason Stone <jason.stone0808@gmail.com>
2018-11-11 21:58:20 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,6 +2,6 @@ def reverse_each_word(sentence)
   sentence.split.collect {|word| word.reverse}.join(" ")
 end
 
-#split
+#split the string(sentence)
 #map/collect each element of the array to its reversed version
 #rejoin the array into a string
